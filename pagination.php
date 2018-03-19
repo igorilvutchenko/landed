2504d3ee13ece3e7c499a2af9a9d3e46b7362eee
Igor Ilvutchenko <corvuscorax84@gmail.com>
2018-03-19 11:57:39 +0200

    change pagination, make array in cilce

--- pagination.php
+++ pagination.php
@@ -5,14 +5,10 @@ $db = mysqli_connect('localhost', 'root', '', 'landed');
 if (!$db)
 	die('Не удалось подключиться к базе данных.');
 
-if(!isset($_GET['page']))
-{
-	$page = 1;
-}
-else
-{
-	$page = $_GET['page'];
-}
+$per_page = 2;
+$page = $_GET['page'] ?? 1;
+$offset = $page * $per_page - $per_page;
+$limit = $per_page;
 
 $sql = "
 		SELECT * 
@@ -20,29 +16,35 @@ $sql = "
 		";
 
 $allUsers = mysqli_num_rows(mysqli_query($db, $sql));
-
 $result = mysqli_query($db, $sql);
-while($row = mysqli_fetch_assoc($result))
-{
-    $users[] =$row;
-}
-
 $counter = ceil($allUsers/$limit);
+
 do {
-	$j = $page*2 - 2;
-		echo '<pre>';
-		var_dump($users[$j]);
-		echo '</pre>';
+	$j = $page*2 - $limit;
 
-		echo '<pre>';
-		var_dump($users[$j+1]);
-		echo '</pre>';
+	$query = "
+		SELECT *
+		FROM `users`
+		ORDER BY `id` ASC
+		LIMIT {$offset}, {$limit}
+		";
 
-		echo $page;
-		if ($page == $counter) {
-			$page = 0;
-		}
-		echo '<a href = "pagination.php?page=' . $page . '">' . $page . '</a>';
-		break;
-	} while ($i < $counter);
+	$rowResult = mysqli_query($db, $query);
+	while($row = mysqli_fetch_assoc($rowResult))
+	{
+	    $users[] =$row;
+	}
+	echo '<pre>';
+	var_dump($users);
+	echo '</pre>';
+	echo "Страница: " . $page . "</br>";
+	if ($page == $counter) {
+		$page = 0;
+	}
+
+	$pagination = $page + 1;
+	echo '<a href = "pagination.php?page=' . $pagination . '">' . $pagination . '</a>';
+	break;
+	} while ($j < $counter);
+	
 mysqli_close($db);
